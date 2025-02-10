17d02652f3d65e4ab32aaae49695c2c973d90474
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-02-10 06:23:22 -0500

    style: Add responsive styles

---
+++ src/app/shared/components/logo-sinapsis/logo-sinapsis.component.ts
@@ -0,0 +1,12 @@
+import { Component, isStandalone } from '@angular/core';
+
+@Component({
+    selector: 'logo-sinapsis',
+    templateUrl: './logo-sinapsis.component.html',
+    styleUrl: 'logo-sinapsis.component.scss',
+    standalone: false
+})
+
+export class LogoSinapsisComponent  {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinerComponent } from './components/spiner/spiner.component';
+import { LogoSinapsisComponent } from './components/logo-sinapsis/logo-sinapsis.component';
 
 
 
 @NgModule({
   declarations: [
     SpinerComponent,
+    LogoSinapsisComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SpinerComponent,
+    LogoSinapsisComponent
   ]
 })
 export class SharedModule { }
